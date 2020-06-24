95d558d3b2f528d3c187985cd2c3a079231f0977
Lauchmitschlauch <67017585+Lauchmitschlauch@users.noreply.github.com>
2020-06-24 11:24:52 +0200

    pythonscript.py
    Geisterspiel

---
+++ pythonscript.py
@@ -0,0 +1,39 @@
+#Dies ist ein kleines "Geisterspiel" viel Spass
+from random import randint
+from time import sleep
+name=input("Was ist dein Name junger Padawan?")
+begruessung="Guten Morgen,junger Padawan haben sie gut geschlafen"
+begruessung = begruessung + ' ' + name + '?'
+
+print(begruessung)
+
+
+du_bist_weise_junger_Padawan=True
+BesiegteSiths = 0
+
+
+
+while du_bist_weise_junger_Padawan:
+    Torzumjedi=randint(1,3)
+    print("SCHWEIZSCHWEIZSCHWEIZSCHWEIZSCHWEIZSCHWEIZSCHWEIZSCHWEIZSCHWEIZSCHWEIZSCHWEIZSCHWEIZSCHWEIZSCHWEIZ")
+    sleep (1)
+    print("Vor dir sind drei Raumkapseln.")
+    sleep (1)
+    print("Hinter einer Tür ist ein Sith, junger Padawan")
+    sleep (1)
+    print("Für welche der Raumkapseln entscheidest du dich?")
+    Raumkapseln = input("1, 2 oder 3? ")
+    Raumkapsel_nummer= int(Raumkapseln)
+    if Raumkapsel_nummer==Torzumjedi:
+        print("ACHHHHHHHTUNG, Jedi, SCHNELL IN DECKUNG")
+        print("Zu langsam, junger Padawan, leider bist du soeben auf dem Todesstern gestorben.")
+        du_bist_weise_junger_Padawan=False
+    else:
+	    print("Gut gemacht"+name)
+
+BesiegteSiths = BesiegteSiths+ 1
+
+print(" ")
+print(name+", du hast leider die Jedi Akademie nicht bestanden.Schande über dich. Nur")
+print(BesiegteSiths)
+print ("Punkte")
